8d69a78df66fbfe9803ef5167ee438ee8d494e12
iedirisi <iedirisi@confederationcollege.ca>
2021-08-06 17:00:36 -0400

    Completed

--- WeatherLab/Program.cs
+++ WeatherLab/Program.cs
@@ -12,7 +12,7 @@ namespace WeatherLab
         {
             var measurements = new WeatherSqliteContext(dbfile).Weather;
 
-            var total_2020_precipitation = ?? TODO ??
+            var total_2020_precipitation = (from data in measurements where data.year == 2020 select data.precipitation).Sum();
             Console.WriteLine($"Total precipitation in 2020: {total_2020_precipitation} mm\n");
 
             //
@@ -39,15 +39,51 @@ namespace WeatherLab
             //      var x = 2;
             // To display as "0002" then:
             //      $"{x:d4}"
-            //
+            
+            var mean_temp = from meanYear in measurements.AsEnumerable()
+                            group meanYear by meanYear.year into resGroup
+                            orderby resGroup.Key
+                            select new
+                            {
+                                Key = resGroup.Key,
+                                hdd = resGroup.Where(r => r.meantemp < 18).Count(),
+                                cdd = resGroup.Where(r => r.meantemp >= 18).Count(),
+                            };
+
             Console.WriteLine("Year\tHDD\tCDD");
 
-            // ?? TODO ??
+            foreach (var meanYear in mean_temp)
+            {
+                Console.WriteLine($"{meanYear.Key}\t{meanYear.hdd}\t{meanYear.cdd}");
+            }
+
+            var variable = from temp in measurements
+                           orderby (temp.maxtemp - temp.mintemp) descending
+                           select new
+                           {
+                               Date = $"{temp.year}-{temp.month:d2}-{temp.day:d2}",
+                               Delta = (temp.maxtemp - temp.mintemp),
+
+                           };
+
 
             Console.WriteLine("\nTop 5 Most Variable Days");
             Console.WriteLine("YYYY-MM-DD\tDelta");
 
-            // ?? TODO ??
+            int count = 0;
+            foreach (var i in variable)
+            {
+                if (count < 5)
+                {
+                    Console.WriteLine($"{i.Date}\t{i.Delta}");
+                    count++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
         }
     }
 }
